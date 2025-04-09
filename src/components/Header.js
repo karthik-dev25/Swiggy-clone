eb1166a9125df00ac60b4e640e2233ea3a32763a
karthikn25 <karthikn25.dev@gmail.com>
2025-04-09 11:44:47 +0530

    Routing Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,39 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router";
 
 const Header = () => {
-  const [btnName,setBtnName] = useState("Login");
+  const [btnName, setBtnName] = useState("Login");
 
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About US</li>
-            <li>Contact US</li>
-            <li>Cart</li>
-            <button onClick={()=>{
-              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
-            }} className="login-btn">{btnName}</button>
-          </ul>
-        </div>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
       </div>
-    );
-  };
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About US</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact US</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+            className="login-btn"
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
