da36b661ccc5ace51635b4b0f47a499510a73416
karthikn25 <karthikn25.dev@gmail.com>
2025-04-10 17:54:30 +0530

    Menu API URL Added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const LOGO_URL="https://cdn.logojoy.com/wp-content/uploads/2018/05/01105943/1954.png"
+export const LOGO_URL="https://cdn.logojoy.com/wp-content/uploads/2018/05/01105943/1954.png";
+
+export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&restaurantId="
