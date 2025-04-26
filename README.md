4c530cdb12393cbb7e0aa5f97596be24cd6ae173
karthikn25 <karthikn25.dev@gmail.com>
2025-04-26 17:43:54 +0530

    Contact Form Added

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Namaste React 
 
+# Create React Application
+ # Create HTML, CSS and JS Files
+ # Connect All the files and Run as HTML Output
+ # Install  react and react-dom libraries
+ # Install Module Bundler Example Parcel
+ # Create a Component in App.js and Create Root Element and Render the App Component
+ # The App Successfully rendered
+
+
 # Redux Toolkit
 
  # install Redux Packages - npm install @reduxjs/toolkit react-redux
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact US Page is here</h1>
+    <div className="w-full flex items-center flex-col">
+      <h1 className="font-bold m-2 p-2">Contact US</h1>
+
+      <div className="flex flex-col">
+        <input className="p-2 m-2 border border-black rounded-lg" placeholder="name" type="text" />
+        <input className="p-2 m-2 border border-black rounded-lg" placeholder="message" type="text" />
+        <button className="p-2 m-2 rounded-lg bg-amber-300 cursor-pointer">Submit</button>
+      </div>
     </div>
   );
 };
